fc747fc69fc14fb90111f72ba067cd1301b8eaf8
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-06-19 18:46:55 +0100

    created election model & index for elections

--- app/controllers/elections_controller.rb
+++ app/controllers/elections_controller.rb
@@ -1,8 +1,10 @@
 class ElectionsController < ApplicationController
   before_action :authenticate_user!
   def index
+    @elections = Election.all
   end
 
   def show
+    @election = Election.find(params[:id])
   end
 end
---
+++ app/models/election.rb
@@ -0,0 +1,3 @@
+class Election < ApplicationRecord
+  belongs_to :user
+end
